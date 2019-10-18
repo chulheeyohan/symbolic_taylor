2a154082be3eeb1ad5e813b69e5296e43ab1ac46
C Lee <32197811+chulheeyohan@users.noreply.github.com>
2019-10-17 23:21:22 -0700

    Add files via upload

---
+++ fapprox.m
@@ -0,0 +1,28 @@
+clc; clear;
+%variable declaration
+global x0 a x
+syms x0 a x
+%
+% function: f(x) = sqrt(x)
+%
+%f = sqrt(x);
+f = str2sym(input('f(x) = ? ... ','s'));
+display('f(x) = '),pretty(f)
+Tapprx = 0;
+% degree, n
+n = 2
+for i = 0:n
+    Tapprx = Tapprx + D(f,i);
+end
+display('Taylor Approximation of f about a at x0 '),pretty(Tapprx)
+a_ = str2num(input('a? ... ','s'));
+x0_ = str2num(input('x0? ... ','s'));
+%a_ = 100;x0_ = 98; 
+disp(['when (a, x0) = (',num2str([a_, x0_]),') is'])
+double(subs(Tapprx,[a, x0],[a_,x0_]))
+
+function g = D(f,n)
+global x0 a x
+g = 1/factorial(n)*subs(diff(f,x,n),x,a)*(x0-a)^n;
+return
+end
